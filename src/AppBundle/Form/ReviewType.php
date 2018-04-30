26b399ba3e4dbedea1cfaa289d1261ecb36d4945
Célia Maure <celimaure@icloud.com>
2018-04-30 14:41:19 +0200

    quete les formulaires symfony

--- src/AppBundle/Form/ReviewType.php
+++ src/AppBundle/Form/ReviewType.php
@@ -2,20 +2,32 @@
 
 namespace AppBundle\Form;
 
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
 
 class ReviewType extends AbstractType
 {
     /**
-     * {@inheritdoc}
+     * {@inheritdoc} including all the fields from Review Entity.
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('text')->add('publicationDate')->add('note')->add('userRated')->add('reviewAuthor');
-    }/**
-     * {@inheritdoc}
+        $builder
+            ->add('text')
+            ->add('publicationDate')
+            ->add('note')
+            ->add('agreeTerms', CheckboxType::class, array('mapped'=>false))
+            ->add('userRated')
+            ->add('reviewAuthor');
+    }
+
+    /**
+     * {@inheritdoc} targeting review entity
      */
     public function configureOptions(OptionsResolver $resolver)
     {
@@ -25,12 +37,10 @@ class ReviewType extends AbstractType
     }
 
     /**
-     * {@inheritdoc}
+     * [@inheritdoc} getName() is now deprecated
      */
     public function getBlockPrefix()
     {
         return 'appbundle_review';
     }
-
-
 }
